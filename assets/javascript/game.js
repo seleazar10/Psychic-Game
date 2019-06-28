2947bde61e30a523031b8303c67dd811dcd47241
Stama Eleazar <eleoatam10@gmail.com>
2019-06-28 14:02:47 -0500

    user input is stored in var - onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,26 @@
 
 
 // counter
+var letters = ["a", "b", "c"];
 var wins = 0;
 var losses = 0;
 var left = 0;
 
 
-document.onclick = function(){
-    console.log('yes')
+//event
+
+document.onkeyup = function(){
+
+    //store user input
+    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userguess);
+
+    
+
+
+
+
+
+
 }
 
