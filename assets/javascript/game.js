ee6abd956d1664d098673728c7d466dc02fc4b99
Stama Eleazar <eleoatam10@gmail.com>
2019-06-28 18:37:39 -0500

    added all letters - working minus guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,8 @@
 
 
 // variables
-var letters = ["a", "b", "c", 'e'];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
+"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 var choices = [];
 
 
@@ -16,14 +17,14 @@ var left = 0;
 
 //event
 
-document.onkeyup = function(){
+document.onkeyup = function () {
 
 
 
-   
+
 
     //store user choice
-    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userguess = String.fromCharCode(event.keyCode).toUpperCase();
     console.log(userguess);
     choices.push(userguess);
 
@@ -38,22 +39,22 @@ document.onkeyup = function(){
     if (userguess == pcguess) {
         left++;
         wins++;
-        console.log('You won! '+ wins+ ' '+ left )
-        
-    }else{
+        console.log('You won! ' + wins + ' ' + left)
+
+    } else {
         left++;
         losses++;
-        console.log('You lost! '+ losses + ' '+ left)
+        console.log('You lost! ' + losses + ' ' + left)
     }
 
 
     //upon start - display stat to user
 
-    var stato = "<p>Guess what letter I'm thinking of</p>" + 
-    "<p>Wins: " + wins + "</p>" + 
-    "<p>Losses: " + losses + "</p>"+ 
-    "<p>Guesses Left: " + (10 - left) + "</p>" +
-    "<p>Your Guesses so far: " + (choices) + "</p>";
+    var stato = "<p>Guess what letter I'm thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + (10 - left) + "</p>" +
+        "<p>Your Guesses so far: " + (choices) + "</p>";
 
 
     //getElement - display
@@ -62,4 +63,6 @@ document.onkeyup = function(){
 
 
 }
+
+
 
